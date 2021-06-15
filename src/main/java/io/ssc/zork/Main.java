977df533d0c5704f7bb016c3f305d43b4de26fb9
IngJirayu <ingapivi@gmail.com>
2021-06-16 00:36:18 +0700

    added scanner

---
+++ src/main/java/io/ssc/zork/Main.java
@@ -0,0 +1,10 @@
+package io.ssc.zork;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.run();
+    }
+}
